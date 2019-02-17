ae8f9dd576739ff8315846afab0d0b0c5bcf4f12
aurebrus <aurebrus@gmail.com>
2019-02-17 20:00:28 +0100

    Alerts (10 minutes)

---
+++ src/models/alerts/constants.py
@@ -0,0 +1,5 @@
+URL = "https://api.mailgun.net/v3/sandboxa08dd2af716e43dfb7bf809afa63488c.mailgun.org/messages"
+API_KEY = "4f9db8d55c116adfee7d41ab978d3cc2-1b65790d-17183ef6"
+FROM = "Mailgun Sandbox <postmaster@sandboxa08dd2af716e43dfb7bf809afa63488c.mailgun.org>"
+ALERT_TIMEOUT = 10
+COLLECTION = "alerts"
