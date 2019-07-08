768cf19e68ee0c511811cee020db98bd03174f0a
Aureliusz <39496956+aurebrus@users.noreply.github.com>
2019-07-08 21:54:43 +0200

    Update constants.py

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,5 +1,5 @@
-URL = "https://api.mailgun.net/v3/sandbox215aa77a43e046c19f52a2d51335f67d.mailgun.org/messages"
-API_KEY = "8902bdc4a912d12bca13d54447a9faeb-acb0b40c-15d73e34"
+URL = ""
+API_KEY = ""
 FROM = "Mailgun Sandbox <postmaster@sandbox215aa77a43e046c19f52a2d51335f67d.mailgun.org>"
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
